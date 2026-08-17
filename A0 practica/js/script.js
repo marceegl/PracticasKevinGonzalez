a421161310690e1b326e2a393446f818d33842b4
enumerates <kevin.gonzalez2027@frech.superate.org>
2026-08-17 10:44:26 -0600

    tretwerwer

---
+++ A0 practica/js/script.js
@@ -0,0 +1,16 @@
+
+function revisar(){
+    let caja = document.getElementById("mensaje")
+
+    //obtenemos el parrafo donde ira el mansaje
+
+    caja.innerHTML="Respuesta correcta! Very gud ;)"
+}
+
+function revisarmal(){
+    let caja = document.getElementById("mensaje")
+
+    //obtenemos el parrafo donde ira el mansaje
+
+    caja.innerHTML="Respuesta mal intanta de nuevo"
+}
